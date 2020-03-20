ce0fbc8d679887f939f80983bd04dba762897bdf
AlexandreLouzada <49560198+AlexandreLouzada@users.noreply.github.com>
2020-03-20 01:01:07 -0300

    Update Lista01Ex08.py

--- envExemplo/Lista01/Lista01Ex08.py
+++ envExemplo/Lista01/Lista01Ex08.py
@@ -2,7 +2,7 @@
 Sejam P(x1,y1) e Q(x2,y2) dois pontos quaisquer do plano. A sua distância é dada por:
 D = ((x2 – x1)**2 + (y2 – y1)**2)**(1/2)
 Elabore um programa que leia as coordenadas dos pontos “P” e “Q”, calcule e escreva
-sua distância.aaaaaa
+sua distância.
 """
 
 x1 = int(input("Digite o valor de x1:"))
