e0c6956eec34c110da130f5a7683500d4ea801cd
AlexandreLouzada <49560198+AlexandreLouzada@users.noreply.github.com>
2020-03-19 22:20:46 -0300

    Update Lista01Ex02.py

--- envExemplo/Lista01/Lista01Ex02.py
+++ envExemplo/Lista01/Lista01Ex02.py
@@ -7,8 +7,8 @@ do sexo feminino e a porcentagem de alunos aprovados.
 sexo1 = int(input('digite a quantidade de alunos do sexo masculino:'))
 sexo2 = int(input('digite a quantidade de alunos do sexo feminino:'))
 totaldealunos= sexo1 + sexo2
-porcentmasc = (totaldealunos * sexo1) / 100
-porcentfemi = (totaldealunos * sexo2) / 100
+porcentmasc = sexo1/totaldealunos * 100
+porcentfemi = sexo2/totaldealunos * 100
 print('o total de alunos é {}'.format(totaldealunos))
 print('a porcentagem de alunos masculinos é de {:.2f}%'.format(porcentmasc))
 print('a porcentagem de alunas femininas é de {:.2f}%'.format(porcentfemi))
