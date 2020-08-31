00c1b7fcbad41ffd7c9d76500b5461cb173caa45
AlexandreLouzada <49560198+AlexandreLouzada@users.noreply.github.com>
2020-08-31 10:58:42 -0300

    Criação de exercicios da unidade 1

--- envExemplo/Lista01/Lista01Ex07.py
+++ envExemplo/Lista01/Lista01Ex07.py
@@ -5,7 +5,7 @@ e o IMC calculado.
 Exemplo: Valores fornecidos pelo teclado: Peso = 60kg e Altura = 1,67m
 Cálculo do IMC = 60 / (1,67)² = 60 / 2,78 = 21,5
 """
-peso= float(input('Digite seu peso:'))
-altura= float(input('Digite sua altura:'))
-imc= (peso / (altura**2))
+peso = float(input('Digite seu peso:'))
+altura = float(input('Digite sua altura:'))
+imc = (peso / (altura ** 2))
 print('Valores fornecidos, Peso= {:.2f}kg, Altura {:.2f}m, cálculo do imc é de {:.2f}'.format(peso, altura, imc))
--- envExemplo/Lista07/Exemplo01/minhaclasse.py
+++ envExemplo/Lista07/Exemplo01/minhaclasse.py
@@ -3,7 +3,9 @@ class Pessoa(object):
         self.nome = ''
         self.idade = 0
         self.peso = 0
+
     def __repr__(self):
         return f'{self.nome} tem {self.idade} anos e pesa {self.peso}Kg.'
+
     def aniversario(self):
         self.idade += 1
