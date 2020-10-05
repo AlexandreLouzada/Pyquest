05a340ab9244f6eb52d0c2e4fd05404dbb6ba999
AlexandreLouzada <49560198+alexandrelouzada@users.noreply.github.com>
2020-10-05 12:23:19 -0300

    Atualização do projeto Pyquest 5/10

---
+++ envExemplo/Lista01/testando.py
@@ -0,0 +1,15 @@
+aprovados = 0
+quantidade = 4
+
+for contador in range(quantidade):
+    aluno = input('digite seu nome:')
+    nota = float(input(f'{aluno} digite sua nota:'))
+    if nota > 7:
+        aprovados += 1
+
+porcentagem_aprovados = aprovados/quantidade * 100
+
+print(f'Quantidade de pessoas aprovadas: {aprovados}')
+print(f'Porcentagem de pessoas aprovadas: {porcentagem_aprovados}')
+
+
