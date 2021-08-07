927a21cd42f8ac7a352fd7d7192ebd364b14d687
AlexandreLouzada <professorlouzada@gmail.com>
2021-08-07 00:38:33 -0300

    Update testando.py

--- envExemplo/Lista01/testando.py
+++ envExemplo/Lista01/testando.py
@@ -1,5 +1,5 @@
 aprovados = 0
-quantidade = 4
+quantidade = 3
 
 for contador in range(quantidade):
     aluno = input('digite seu nome:')
@@ -10,6 +10,6 @@ for contador in range(quantidade):
 porcentagem_aprovados = aprovados/quantidade * 100
 
 print(f'Quantidade de pessoas aprovadas: {aprovados}')
-print(f'Porcentagem de pessoas aprovadas: {porcentagem_aprovados}')
+print(f'Porcentagem de pessoas aprovadas: {porcentagem_aprovados:.2f}')
 
 
