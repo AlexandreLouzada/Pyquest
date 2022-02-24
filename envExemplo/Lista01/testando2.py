7405371dc4ba9168242483a47908312e2388de78
AlexandreLouzada <professorlouzada@gmail.com>
2022-02-24 18:50:10 -0300

    Create testando2.py

---
+++ envExemplo/Lista01/testando2.py
@@ -0,0 +1,11 @@
+import math
+
+X=int(input('Digite o primeiro valor:')) 
+Y=int(input('Digite o  segundo valor:')) 
+
+P = math.pow(X, Y);
+
+print(f'Novo valor de P:{P}')
+
+print(6.02214076 * 10 ** 23)
+
