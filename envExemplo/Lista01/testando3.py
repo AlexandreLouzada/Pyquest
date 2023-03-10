21198686949f2e70bbf278dbf69245bc60b97230
AlexandreLouzada <professorlouzada@gmail.com>
2023-03-10 17:06:32 -0300

    Update testando3.py
    Testando uso do comando match case

--- envExemplo/Lista01/testando3.py
+++ envExemplo/Lista01/testando3.py
@@ -1,10 +1,12 @@
-valor = 0
-match valor:
-    case (1):
-        print("Domingo")
-    case (2):
-        print("Segunda")
-    case (3):
-        print("Terça")
-    case _:
-        print("Valor inválido")
+sigla = 'SP'
+match sigla:
+    case 'RJ':
+        print('Rio de Janeiro')
+    case 'SP':
+        print('São Paulo')
+    case 'MG':
+        print('Minas Gerais')
+    case 'ES':
+        print('Espírito Santo')
+    case other:
+        print('A sigla não foi identificada como parte da região sudeste')
