fe0cbb901c007483bdbb6b1312d37634929e2820
AlexandreLouzada <professorlouzada@gmail.com>
2023-03-10 15:36:39 -0300

    Atualizando lista 1

---
+++ envExemplo/Lista01/testando3.py
@@ -0,0 +1,10 @@
+valor = 0
+match valor:
+    case (1):
+        print("Domingo")
+    case (2):
+        print("Segunda")
+    case (3):
+        print("Terça")
+    case _:
+        print("Valor inválido")
